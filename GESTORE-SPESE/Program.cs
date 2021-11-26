36a2eb04f23644922d03835e049504700c9feecc
Edoardo-Galantini <71138907+Edoardo-Galantini@users.noreply.github.com>
2021-11-26 22:50:38 +0100

    Add files via upload

---
+++ GESTORE-SPESE/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GESTORE_SPESE.States;
+
+namespace GESTORE_SPESE
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto di ingresso principale dell'applicazione.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // Creation of the StateContext object for the operation of the State Pattern
+            StateContext ctx = new StateContext();
+
+            // State update 
+            ctx.UpdateState();
+            ctx.UpdateState();
+            ctx.UpdateState();
+        }
+    }
+}
